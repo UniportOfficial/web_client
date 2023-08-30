76bb6353f6fa33c54011c04b7e9154da6e9d51cf
Duhoon <eclip6@naver.com>
2023-08-30 16:30:09 +0900

    feat: add component Navigation

---
+++ src/components/navigation/Navigation.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faHome, faComment, faPlusSquare, faUser, faBars } from "@fortawesome/free-solid-svg-icons"
+import { 
+    faUser as faUserReuglar,
+    faComment as faCommentRegular
+} from "@fortawesome/free-regular-svg-icons"
+
+export default function Navigation(){
+    return(
+        <nav className="fixed bottom-0 py-4 nav-wrapper w-full flex justify-evenly items-center ">
+            <div className="text-center text-2xl">
+                <button><FontAwesomeIcon icon={faHome}/></button>
+            </div>
+            <div className="text-center text-2xl">
+                <button><FontAwesomeIcon icon={faCommentRegular}/></button>
+            </div>
+            <div className="slider text-4xl text-slate-500">
+                <button>
+                    <FontAwesomeIcon icon={faPlusSquare}/>
+                </button>
+            </div>
+            <div className="text-center text-2xl">
+                <button><FontAwesomeIcon icon={faUserReuglar}/></button>
+            </div>
+            <div className="text-center text-2xl">
+                <button><FontAwesomeIcon icon={faBars}/></button>
+            </div>
+        </nav>
+    )
+}
